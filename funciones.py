e52196badba656dff7bb4f8740cc57abc793e5a3
Zibit0 <ziebold.lzm@gmail.com>
2026-05-20 20:47:45 -0400

    buena suerte compañero

--- funciones.py
+++ funciones.py
@@ -1,11 +1,24 @@
 def menu():
     print('''
 1.- Sumar
-2.- Restar''')
+2.- Restar
+3.- Multiplicar
+4.- Dividir''')
 
 def calculadora(numero1, numero2, opcion):
     if opcion == 1: #suma
-        resultado = numero1 + numero2 
+        resultado = numero1 + numero2
+        operacion = "+" 
+        adjetivo = "Sopenco"
     elif opcion == 2: #resta
         resultado = numero1 - numero2
-    return resultado
+        operacion = "-"
+        adjetivo = "Aweonao"
+    elif opcion == 3: #multiplicación
+        resultado = numero1 * numero2
+        operacion = "x"
+    elif opcion == 4: #división
+        resultado = numero1 / numero2
+        operacion = "/"
+
+    return resultado, operacion, adjetivo
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import funciones as fun
 
-
-print('Bienvenido a la calculadora magica!')
-primer_numero = int(input('Por favor ingrese su primer numero!: '))
-segundo_numero = int(input('Por favor ingrese su segundo numero!: '))
 while True:
+    print('Bienvenido a la calculadora magica!')
+    primer_numero = int(input('Por favor ingrese su primer numero!: '))
+    segundo_numero = int(input('Por favor ingrese su segundo numero!: '))
+
     fun.menu()
     decision = int(input('Por favor escoja la opción que desea: '))
-    respuesta = fun.calculadora(primer_numero,segundo_numero,decision)
-    print(f'El resultado de su operacion de {primer_numero} con {segundo_numero} = {respuesta}')
+    respuesta, operacion, adjetivo = fun.calculadora(primer_numero,segundo_numero,decision)
+    print(f'{primer_numero} {operacion} {segundo_numero} = {respuesta}, {adjetivo if adjetivo else ""}')
