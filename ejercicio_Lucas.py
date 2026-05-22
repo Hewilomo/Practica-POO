fe80c726310eb9a32a4adad0b796ee9be2628d6c
Zibit0 <ziebold.lzm@gmail.com>
2026-05-22 01:24:59 -0400

    empezando cacho

--- ejercicio_Lucas.py
+++ ejercicio_Lucas.py
@@ -2,11 +2,12 @@
 # - te de el promedio ponderado 
 # - te entregue la respuesta si es que el alumno aprobo o reprobo
 # - el ramo se pasa con un 4.0 para arriba
+
 nota1 = float(input("Ingrese su primera nota: "))
 nota2 = float(input("Ingrese su segunda nota: "))
 nota3 = float(input("Ingrese su tercera nota: "))
 import funciones as fun
-fun.calculadora_notas()
+fun.calculadora_notas(nota1=nota1, nota2=nota2, nota3=nota3)
 
 # Actividad Bonus:
 # Muestrale al usuario un menú que le permita jugar cacho
@@ -16,3 +17,6 @@ fun.calculadora_notas()
 # La función debe retornar dos resultados:
 # 1 un listado con los 6 dados
 # 1 resultado del total de los dados :)
+
+menu = input("Desea jugar cacho contra la CPU? s/n: ") 
+if menu == "s":
