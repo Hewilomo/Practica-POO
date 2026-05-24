7d89924b18f7efe441ead084727dc8a48614e3bb
ealesaph <enrique.alegria02@inacapmail.cl>
2026-05-24 17:26:06 -0400

    baby first steps

--- diccionario.py
+++ diccionario.py
@@ -1,4 +1,3 @@
-
 list_dic ={
         "nombre": "",
         "vida": "",
@@ -9,3 +8,10 @@ list_dic ={
 nuevo_char = input('Ingrese su nombre: ')
 list_dic["nombre"] = nuevo_char
 print(list_dic)
+
+#En base a este ejemplo crear un menu de creación 
+
+#getter tu obtienes datos para modificarlos en ESA instancia
+#setter tu privatizas los datos, haciendolos imposible de alterar y LEER si es que no lo permites
+# Getter y Setter permiten Encapsular datos, esto permite que la persona solo pueda modificar PERSONAJE no base de datos
+#GETTER Y SETTERS SON EXCLUSIVOS DE PROGRAMACIÓN ORIENTADA AL OBJETO (POO)
