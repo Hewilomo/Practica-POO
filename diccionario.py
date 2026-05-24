3a6640afe18debd963250fdd083bc4f0b5a55533
ealesaph <enrique.alegria02@inacapmail.cl>
2026-05-24 17:20:18 -0400

    baby first steps

---
+++ diccionario.py
@@ -0,0 +1,11 @@
+
+list_dic ={
+        "nombre": "",
+        "vida": "",
+        "ataque": "",
+        "inteligencia": "", 
+        }
+
+nuevo_char = input('Ingrese su nombre: ')
+list_dic["nombre"] = nuevo_char
+print(list_dic)
